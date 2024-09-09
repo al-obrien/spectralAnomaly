c2242c17a348d78cd1f1f6727c573acca33c9f5d
al-obrien <allen.g.obrien@gmail.com>
2024-09-09 10:20:15 -0600

    Add helper function for threshold application to scores

--- R/utils.R
+++ R/utils.R
@@ -22,3 +22,20 @@ avg_sliding_window <- function(x, window) {
   out[1:up2window] <- first_set[1:up2window] / 1:up2window
   out
 }
+
+#' Apply threshold to anomaly score
+#'
+#' A helper function that wraps around \code{quantile} to apply a threshold to anomaly scores.
+#'
+#' @param x Numeric vector of anomaly scores (e.g. created by \code{anomaly_score}).
+#' @param threshold Numeric value to determine the threshold to flag outliers among the score.
+#' @param ... Additional parameters passed to \code{quantile}.
+#'
+#' @returns Logical vector referencing which, if any, of the provided values are outliers.
+#'
+#' @examples
+#' test_data <- c(1,2,3,4,5,100,5,4,3,2,1)
+#' anomaly_thresh(test_data, 0.99)
+#' @export
+anomaly_thresh <- function(x, threshold = 0.99, ...) {
+  x > stats::quantile(x, prob = threshold, ...)
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,3 +1,5 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Threshold helper returns expected vector...", {
+  sal_map <- c(0.008132905, 0.002205079, 0.014745095, 0.024349918, 0.023928495, 0.982835279, 0.023928495, 0.024349918, 0.014745095, 0.002205079, 0.008132905)
+  expect_equal(anomaly_thresh(sal_map, threshold = 0.95),
+               c(F, F, F, F, F, T, F, F, F, F, F))
 })
