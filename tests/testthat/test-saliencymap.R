df09936096a52026e7ce5f574149df38a45b3faa
al-obrien <allen.g.obrien@gmail.com>
2024-09-09 10:20:28 -0600

    Add basic tests for saliency map

---
+++ tests/testthat/test-saliencymap.R
@@ -0,0 +1,13 @@
+test_that("Obvious outlier detection...", {
+  time_series <- c(1,2,3,4,5,100,5,4,3,2,1)
+  sal_map <- saliency_map(time_series)
+  expect_equal(sal_map > quantile(sal_map, prob = 0.95),
+               c(F, F, F, F, F, T, F, F, F, F, F))
+})
+
+test_that("Obvious no anomaly detection...", {
+  time_series <- c(1,2,3,4,5,5,5,4,3,2,1)
+  sal_map <- saliency_map(time_series)
+  expect_equal(sal_map > quantile(sal_map, prob = 0.95),
+               c(F, F, F, F, F, F, F, F, F, F, F))
+})
