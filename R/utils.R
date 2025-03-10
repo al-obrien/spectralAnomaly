ebe73688876bba831519494b8ce030bbb05413ca
al-obrien <allen.g.obrien@gmail.com>
2025-03-09 19:04:09 -0600

    Typos and more tests

--- R/utils.R
+++ R/utils.R
@@ -59,13 +59,13 @@ anomaly_thresh <- function(x, threshold = 0.99, ...) {
 #' @noRd
 var_sliding_window <- function(x, window) {
   # Deal with initial set of variances up to window
-  up2window <- window-1
+  up2window <- window - 1
   idx_v <- sapply(X = 1:up2window, FUN = function(idx) seq_along(1:idx), simplify = TRUE)
   first_set_v <- sapply(idx_v, function(idx) var(x[idx]))
 
   # Use embedding and concatenation of vars
-  w_var <- apply(embed(x, window), MARGIN = 1, var)
-  return(c(start_var, w_var))
+  w_var <- apply(embed(x, window), MARGIN = 1, stats::var)
+  return(c(first_set_v, w_var))
 }
 
 
@@ -83,12 +83,12 @@ var_sliding_window <- function(x, window) {
 #' @inheritParams anomaly_score
 #' @param n Integer value for number of anomalies to apply
 #' @export
-add_anomaly <- function(ts, n, score_window, spec_window) {
+add_anomaly <- function(x, n, score_window, spec_window) {
   n_idx <- sample(2:length(x), n, replace = FALSE) # Dont pick first point as var is NA
   local_avg <- avg_sliding_window(x, score_window) # should be less than spec
   w_avg <- avg_sliding_window(x, spec_window)
   w_var <- var_sliding_window(x, spec_window)
-  anomaly_v <- ((local_avg[n_idx] + w_avg[n_idx]) * (1 + w_var[n_idx]) * rnorm(n)) + (x[n_idx])
+  anomaly_v <- ((local_avg[n_idx] + w_avg[n_idx]) * (1 + w_var[n_idx]) * stats::rnorm(n)) + (x[n_idx])
   x[n_idx] <- anomaly_v
   x
 }
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -3,3 +3,15 @@ test_that("Threshold helper returns expected vector...", {
   expect_equal(anomaly_thresh(sal_map, threshold = 0.95),
                c(F, F, F, F, F, T, F, F, F, F, F))
 })
+
+test_that("var_sliding_window computes as expected...", {
+  expect_equal(round(var_sliding_window(1:20, 4),2),
+               c(NA, 0.50, 1.00, rep(1.67, 17)))
+})
+
+# test_that("Add an anomaly...", {
+#   withr::local_seed(1980)
+#   expect_equal(add_anomaly(c(1,8,10,11,20,25,50,50,20,20,20,5), 2, 5, 9),
+#                c(1, 8 , 10, 11, 20, 1574.563, 50, 50, 20, 20 ,20, -4071.679))
+# })
+
